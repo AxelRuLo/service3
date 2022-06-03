abd9c5051228760b60e2f0430b2d9cc49f2674dd
Axel Ruiz L <developeraxrulo@gmail.com>
2022-06-03 10:58:23 -0500

    cambio send

--- routes/calculadora.js
+++ routes/calculadora.js
@@ -7,7 +7,7 @@ ruta.get("/:numero1/:numero2", function (req, res) {
         let numero1 = parseInt(req.params.numero1)
         let numero2 = parseInt(req.params.numero2)
         let suma = numero1 + numero2
-        res.send({suma});
+        res.send(suma);
 
     } catch (error) {
         res.send("erro de numeros")
